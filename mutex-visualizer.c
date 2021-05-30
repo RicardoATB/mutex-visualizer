0b2601ab190e7ad61a94171cab92d8b153487ba8
RicardoATB <ricardoatb@gmail.com>
2021-05-30 21:41:17 +0000

    test 4

--- mutex-visualizer.c
+++ mutex-visualizer.c
@@ -27,53 +27,11 @@
 /* GPIO mask */
 #define MASK_ALL_OUTPUT	0x00 // this bitmask will set all I/O pins of one bank to output (0)
 
-
-
 static uint16_t spi_delay_usecs = 0;
 static uint32_t spi_speed_hz = 1000000;
 static uint16_t spi_bits_per_word = 8;
 int spi_fd;
 int status;
-
-// The binary data that describes the LED state for each symbol
-// A(top)         B(top right) C(bottom right)  D(bottom)
-// E(bottom left) F(top left)  G(middle)        H(dot)
-const unsigned char symbols[16] = {                   //(msb) HGFEDCBA (lsb)
-     0b00111111, 0b00000110, 0b01011011, 0b01001111,  // 0123
-     0b01100110, 0b01101101, 0b01111101, 0b00000111,  // 4567
-     0b01111111, 0b01100111, 0b01110111, 0b01111100,  // 89Ab
-     0b00111001, 0b01011110, 0b01111001, 0b01110001   // CdEF
-};
-
-const unsigned char ratb_symbols[10] = { GREEN_ADDR,
-					 IODIRA, MASK_ALL_OUTPUT, 
-					 GREEN_ADDR,
-					 IODIRB, MASK_ALL_OUTPUT,
-					 GREEN_ADDR,
-					 GPIOA, 0xFF, 0xFF };
-
-int transfer(int fd, unsigned char send[], unsigned char rec[], int len){
-   struct spi_ioc_transfer transfer;        //the transfer structure
-   transfer.tx_buf = (unsigned long) send;  //the buffer for sending data
-   transfer.rx_buf = (unsigned long) rec;   //the buffer for receiving data
-   transfer.len = len;                      //the length of buffer
-   transfer.speed_hz = 1000000;             //the speed in Hz
-   transfer.bits_per_word = 8;              //bits per word
-   transfer.delay_usecs = 0;                //delay in us
-   transfer.cs_change = 0;       // affects chip select after transfer
-   transfer.tx_nbits = 0;        // no. bits for writing (default 0)
-   transfer.rx_nbits = 0;        // no. bits for reading (default 0)
-   transfer.pad = 0;             // interbyte delay - check version
-
-   // send the SPI message (all of the above fields, inc. buffers)
-   int status = ioctl(fd, SPI_IOC_MESSAGE(1), &transfer);
-   if (status < 0) {
-      perror("SPI: SPI_IOC_MESSAGE Failed");
-      return -1;
-   }
-   return status;
-}
-
 
 void write_spi(uint8_t slave_addr, uint8_t reg, uint8_t data) {
     	struct spi_ioc_transfer spi;
@@ -110,7 +68,6 @@ int main(){
    //uint8_t mode = 3;                       // SPI mode 3
    uint8_t mode = 0;
 
-/**********************************************************************************/
    // The following calls set up the SPI bus properties
    if ((fd = open(SPI_PATH, O_RDWR))<0){
       perror("SPI Error: Can't open device.");
@@ -125,36 +82,7 @@ int main(){
       return -1;
    }
    printf("SPI Mode is: %d\n", mode);
-/**********************************************************************************/
-   /*
-   for (i=0; i<=15; i++)
-   {
-      // This function can send and receive data, just sending now
-      if (transfer(fd, (unsigned char*) &symbols[i], &null, 1)==-1){
-         perror("Failed to update the display");
-         return -1;
-      }
-      printf("%4d\r", i);   // print the number in the terminal window
-      fflush(stdout);       // need to flush the output, no \n
-      usleep(2000000);
-   }
-   close(fd);               // close the file
-   */
-
-/**********************************************************************************/
-/*
-   for (i = 0; i <= 9; i++)
-   {
-   	// This function can send and receive data, just sending now
-   	if (transfer(fd, (unsigned char*) &ratb_symbols[i], &null, 1)==-1) {
-   		perror("Failed to update the display");
-        	return -1;
-	}
-   }
    
-   close(fd);               // close the file
-   */
-/**********************************************************************************/
    // The following calls set up the SPI bus properties
    if ((spi_fd = open(SPI_PATH, O_RDWR))<0){
       perror("SPI Error: Can't open device");
@@ -189,11 +117,10 @@ int main(){
 	write_spi(OSC_ADDR, IODIRB, 0x00);        //		    port_B ==> output
 	
 	
-
 	write_spi(GREEN_ADDR, GPIOA, 0xFF);
 	write_spi(GREEN_ADDR, GPIOB, 0x00);
 	
-	write_spi(RED_ADDR, GPIOA, 0x00);
+	write_spi(RED_ADDR, GPIOA, 0xFF);
 	write_spi(RED_ADDR, GPIOB, 0xFF);
 
 	write_spi(OSC_ADDR, GPIOA, 0xFF);
