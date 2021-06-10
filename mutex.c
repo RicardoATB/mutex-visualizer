7efb274073b06888ad98728ab8734a675fa8085d
RicardoATB <ricardoatb@gmail.com>
2021-06-10 20:03:55 +0000

    minor improvements

--- mutex.c
+++ mutex.c
@@ -1,7 +1,11 @@
 
-// Description: 
+// Description: Embedded system running real-time software (PREEMPT_RT Linux kernel) to 
+// show how MUTEX and POSIX threads work. Visual output using LEDs (3 x 16-bit shift 
+// register IC + SPI protocol) and an oscilloscope (X-Y mode).
+//
 // Author: Ricardo Augusto Teixeira Barbosa
-// Compiling instructions: $ gcc mutex-prog.c -o mutex -lpthread
+//
+// Compiling instructions: $ gcc mutex.c -o mutex -lpthread
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,7 +18,7 @@
 #include <time.h>
 #include <string.h>
 #include <termios.h>
-/*------------------------------------------------------------------------------------------------*/
+
 #define SPI_PATH "/dev/spidev1.0"
 
 /* Slave Addresses */
@@ -32,7 +36,7 @@
 /* GPIO mask */
 #define MASK_ALL_OUTPUT	0x00 // this bitmask will set all I/O pins of one bank to output (0)
 #define USLEEP_TIME 350000
-/*------------------------------------------------------------------------------------------------*/
+
 static uint16_t spi_delay_usecs = 0;
 static uint32_t spi_speed_hz = 1000000;
 static uint16_t spi_bits_per_word = 8;
@@ -43,7 +47,7 @@ int turn_leds_on = 1;
 int speed = 100;
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-/*------------------------------------------------------------------------------------------------*/
+
 void write_spi(uint8_t slave_addr, uint8_t reg, uint8_t data) {
     	struct spi_ioc_transfer spi;
 	uint8_t spi_buffer[3];
@@ -60,7 +64,7 @@ void write_spi(uint8_t slave_addr, uint8_t reg, uint8_t data) {
 
    	ioctl(spi_fd, SPI_IOC_MESSAGE(1), &spi);
 }
-/*------------------------------------------------------------------------------------------------*/
+
 // BeagleBone Black pins: P9_17 (~CS), P9_18 (MOSI), P9_22(CLK)
 int config_SPI_pins_BBB()
 {
@@ -72,8 +76,7 @@ int config_SPI_pins_BBB()
     ret = WEXITSTATUS(status); // if no errors occur, WEXITSTATUS returns 0
     return ret;
 }
-/*------------------------------------------------------------------------------------------------*/
-// https://stackoverflow.com/questions/13460934/strcpy-using-pointers
+
 // Copying char* to another char*
 void str_cpy(char *dst, const char *src) {
    while (*src != '\0') {
@@ -81,7 +84,7 @@ void str_cpy(char *dst, const char *src) {
    }
    *dst = '\0';
 }
-/*------------------------------------------------------------------------------------------------*/
+
 void update_leds(int step, int led_number) {
     char *aux_led_color = malloc(sizeof(char)*(strlen(led_color) + 1));
     
@@ -93,7 +96,7 @@ void update_leds(int step, int led_number) {
     printf("L_%d_%s ", led_number, aux_led_color);
     free (aux_led_color);
 }
-/*------------------------------------------------------------------------------------------------*/
+
 void* change_speed(void* arg) {
     for(;;) {
         while (turn_leds_on == 1) {
@@ -110,8 +113,7 @@ void* change_speed(void* arg) {
         }
     }
 }
-/*------------------------------------------------------------------------------------------------*/
-// https://stackoverflow.com/questions/9547868/is-there-a-way-to-get-user-input-without-pressing-the-enter-key
+
 void* keyboard_listener(void* arg) {
 
 	int ret_keyboard = 2021;
@@ -130,7 +132,6 @@ void* keyboard_listener(void* arg) {
     for (;;) {
         c = getchar();
         if (c != 'q') {
-        //if (c == 'S') {
             /* The pthread_mutex_trylock() function shall be equivalent to pthread_mutex_lock(), 
              * except that if the mutex object referenced by mutex is currently locked (by any 
              * thread, including the current thread), the call shall return immediately. */
@@ -146,53 +147,19 @@ void* keyboard_listener(void* arg) {
                 pthread_mutex_unlock(&lock);
             }
         }
-		/*
-        else if (c == 's') {                                                                             
-            if (pthread_mutex_trylock(&lock) != 0) {                                                
-                led_color = "R";                                                                    
-                printf("...I can't change speed! MUTEX BLOCK! speed is %d :(\n", speed);
-            }                                                                                       
-            else {                                                                                  
-                led_color = "G";                                                                    
-                speed--;                                                                            
-                turn_leds_on = 1;                                                                           
-                printf("...I CAN change speed. MUTEX UNBLOCK! speed is %d :)\n", speed);                              
-                pthread_mutex_unlock(&lock);                                                        
-            }                                                                                       
-        }
-		*/
         else {
-            //printf("Error: command not recognized\n")
 			printf("\nEXIT\n");
 			pthread_exit(&ret_keyboard);
         }
     }
 }
-/**************************************************************************************************/
+
 int main() {
 
-	int fd, i;					// file handle and loop counter
-	int err;
-	unsigned char null=0x00;		// sending only a single char
-	//uint8_t mode = 3;			// SPI mode 3
-	uint8_t mode = 0;
+	int err;					// error return value
+	unsigned char null=0x00;	// sending only a single char
+	uint8_t mode = 0;			// SPI mode 0
 
-    // The following calls set up the SPI bus properties
-    if ((fd = open(SPI_PATH, O_RDWR))<0){
-		perror("SPI Error: Can't open device.");
-      return -1;
-   	}
-   	if (ioctl(fd, SPI_IOC_WR_MODE, &mode)==-1){
-      	perror("SPI: Can't set SPI mode.");
-      	return -1;
-   	}
-   	if (ioctl(fd, SPI_IOC_RD_MODE, &mode)==-1){
-      	perror("SPI: Can't get SPI mode.");
-      	return -1;
-   	}
-   
-	printf("SPI Mode is: %d\n", mode);
-   
    	// The following calls set up the SPI bus properties
    	if ((spi_fd = open(SPI_PATH, O_RDWR))<0){
       	perror("SPI Error: Can't open device");
@@ -217,28 +184,29 @@ int main() {
 	
    	write_spi(GREEN_ADDR, IOCON_BANK0, 0x08); // set HAEN bit to allow multiple ICs (slave addr)
 
-	write_spi(GREEN_ADDR, IODIRA, 0x00);      // GREEN_LED_DRIVER IC: port_A ==> output
-	write_spi(GREEN_ADDR, IODIRB, 0x00);      //		          port_B ==> output
+	write_spi(GREEN_ADDR, IODIRA, 0x00);      // GREEN_LED_DRIVER IC:	port_A ==> output
+	write_spi(GREEN_ADDR, IODIRB, 0x00);      //		          		port_B ==> output
 	
-	write_spi(RED_ADDR, IODIRA, 0x00);        // RED_LED_DRIVER IC: port_A ==> output
-	write_spi(RED_ADDR, IODIRB, 0x00);        //		        port_B ==> output
+	write_spi(RED_ADDR, IODIRA, 0x00);        // RED_LED_DRIVER IC:		port_A ==> output
+	write_spi(RED_ADDR, IODIRB, 0x00);        //		        		port_B ==> output
 	
-	write_spi(OSC_ADDR, IODIRA, 0x00);        // OSC_DRIVER IC: port_A ==> output
-	write_spi(OSC_ADDR, IODIRB, 0x00);        //		    port_B ==> output
+	write_spi(OSC_ADDR, IODIRA, 0x00);        // OSC_DRIVER IC:			port_A ==> output
+	write_spi(OSC_ADDR, IODIRB, 0x00);        //		    			port_B ==> output
 	
 	
-	write_spi(GREEN_ADDR, GPIOA, 0xFF);
+	// Setting outputs from 3 shift-registers:
+	write_spi(GREEN_ADDR, GPIOA, 0x00);
 	write_spi(GREEN_ADDR, GPIOB, 0x00);
 	
-	write_spi(RED_ADDR, GPIOA, 0xFF);
-	write_spi(RED_ADDR, GPIOB, 0xFF);
+	write_spi(RED_ADDR, GPIOA, 0x00);
+	write_spi(RED_ADDR, GPIOB, 0x00);
 
-	write_spi(OSC_ADDR, GPIOA, 0xFF);
+	write_spi(OSC_ADDR, GPIOA, 0x00);
 	write_spi(OSC_ADDR, GPIOB, 0x00);
 	
 	close(spi_fd);
-	/***************************************************************************/
-    pthread_t speed_thread;
+    
+	pthread_t speed_thread;
     pthread_t keyboard_listener_thread;
 
     err = pthread_create(&speed_thread, NULL, change_speed, NULL);
